7e73eef35a97fd18a415da2bc247c4d27224b1f4
whitzhu <whitzhu@gmail.com>
2017-06-28 23:19:54 -0700

    [NewBulletEntry] update to reuse <Entry/>

--- src/components/NewBulletEntry/NewBulletEntry.js
+++ src/components/NewBulletEntry/NewBulletEntry.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
+import Entry from '../Entry/Entry';
+
 require('./newBulletEntry.scss');
 
 export default class NewBulletEntry extends Component {
   constructor(props) {
     super(props);
- 
-    this.state = { text: this.props.text || '' };
 
+    this.state = { 
+      text: this.props.text || '' ,
+      entryType: this.props.type || 'task',
+      placeholder: this.props.placeholder || '',
+    };
+
+    this.changeEventType = this.changeEventType.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  changeEventType() {
+    console.log('changeEntryType');
   }
 
   handleChange(e) {
@@ -19,22 +30,22 @@ export default class NewBulletEntry extends Component {
   handleSubmit(e) {
     const text = e.target.value.trim();
     if (e.which === 13) {
-      console.log('enter', text);
+      this.props.newEntry({ date: 'June 19, 2017', plan: { entryType: 'task', text: this.state.text } });
       this.setState({ text: '' });
+      this.props.fetchEntry();
     }
   }
 
   render() {
+    const placeholder = {
+     task: 'Add Task: Buy Milk',
+     event: 'Add Event: 10AM Meeting with Bob',
+     note: 'Add Note: Mark does not like pickles!',
+    };
+
     return(
-      <div className="new-entry-container">
-        <input
-          type="text"
-          className="new-entry"
-          placeholder="+ New Entry"
-          value={this.state.text}
-          onChange={this.handleChange}
-          onKeyDown={this.handleSubmit}
-        />
+      <div className="new-entry-container" >
+        <Entry type={this.state.entryType} edit placeholder={this.state.placeholder} />
       </div>
     );
   }
