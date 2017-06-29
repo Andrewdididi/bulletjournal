f8395b9f86e1ad914dd8584140c6759197a46897
whitzhu <whitzhu@gmail.com>
2017-06-28 23:16:56 -0700

    [Day] Day component now renders gratitude, goals, lessons and wins component

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import ProTypes from 'prop-types';
 
 import DateHeader from '../DateHeader/DateHeader';
-import BulletEntry from '../BulletEntry/BulletEntry';
-import NewBulletEntry from '../NewBulletEntry/NewBulletEntry';
+import Entry from '../Entry/Entry';
+import DailyGratitude from '../DailyGratitude/DailyGratitude';
+import DailyQuotes from '../DailyQuotes/DailyQuotes';
+import DailyPlan from '../DailyPlan/DailyPlan';
+import DailyLessonsLearned from '../DailyLessonsLearned/DailyLessonsLearned';
+import DailyWins from '../DailyWins/DailyWins';
+
+import dateConverter from '../../lib/dateConverter';
 
 require('./day.scss');
 
-const Day = ({ date }) => (
+const Day = ({ entry, newEntry, fetchEntry }) => (
   <div className="day-container">
-    <DateHeader date={date} />
-    <BulletEntry type="task" text="buy milk" />
-    <BulletEntry type="task" text="take out the trash by tonight" />
-    <BulletEntry type="migrate" text="reschedule dinner with friends" />
-    <BulletEntry type="event" text="buy bread" />
-    <NewBulletEntry />
+    <DateHeader date={dateConverter(entry.date)} />
+    <DailyQuotes quote="Anything is possible" />
+    <DailyGratitude data={entry.gratitude} />
+    <DailyPlan data={entry.plan} />
+    <DailyLessonsLearned data={entry.lessons} />
+    <DailyWins data={entry.wins} />
   </div>
 );
 
