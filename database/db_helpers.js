7531d33512f3b175d4b245029f290165fbc69d2a
whitzhu <whitzhu@gmail.com>
2017-06-28 23:22:04 -0700

    [DB] update helper function for new dailyEntry schema

--- database/db_helpers.js
+++ database/db_helpers.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
-const Entry = require('./models/entry');
+const DailyEntry = require('./models/dailyEntry');
 
-module.exports.storeEntry = entry => (
-  Entry(entry).save()
+module.exports.storeEntry = (entry) => {
+  const query = { date: entry.date };
+  const update = { $push: { gratitude: entry.gratitude, plan: entry.plan, lessons: entry.lessons, wins: entry.wins } };
+  const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+  return DailyEntry.update(query, update, options)
   .then(result => console.log('SUCCESS STORING INTO DB:', result))
   .error(err => console.log('ERROR STORING INTO DB:', err))
-);
+
+
+};
 
 module.exports.fetchEntry = () => (
-  Entry.find()
+  DailyEntry.find()
   .then(result => result)
   .error(err => console.log('ERROR FETCHING DB:', err))
 );
-
