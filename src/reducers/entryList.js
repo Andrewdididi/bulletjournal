aa91af4ea908b82250dcdfe8fe6a0fd21947df62
whitzhu <whitzhu@gmail.com>
2017-06-30 23:11:50 -0700

    [redux] update entryList reducer return state

--- src/reducers/entryList.js
+++ src/reducers/entryList.js
@@ -8,21 +8,13 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case types.REQUEST_FETCH_ENTRY:
-      return Object.assign({}, state, {
-        isLoading: true,
-      });
+      return { ...state, isLoading: true };
     case types.FETCH_ENTRY_SUCCESS:
-      return Object.assign({}, state, {
-        isLoading: false,
-        entries: [...action.entry],
-      });
+      return { ...state, isLoading: false, entries: [...action.entry] };
     case types.FETCH_ENTRY_FAILURE:
       return action.error;
     case types.ENTRY_ITEM_CHANGE:
-      return Object.assign({}, state, {
-        entries: action.entries,
-      });
-      console.log('ENTRY_ITEM_CHANGE==REDUCER==entryList===', entryList);
+      return { ...state, entries: action.entries };
     default:
       return state;
   }
